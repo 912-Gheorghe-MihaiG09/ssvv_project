a9954c9826ade63e8fed642556fd25d7a93ad114
912-Gheorghe-MihaiG09 <91673296+912-Gheorghe-MihaiG09@users.noreply.github.com>
2024-04-28 16:32:37 +0300

    Add files via upload

---
+++ src/main/java/com/mihaigheorghe/validation/ValidationException.java
@@ -0,0 +1,8 @@
+package com.mihaigheorghe.validation;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String exception) {
+        super(exception);
+    }
+}
+
